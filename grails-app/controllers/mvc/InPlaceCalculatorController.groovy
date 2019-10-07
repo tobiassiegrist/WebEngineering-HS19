36acdfadd7be918b1ecdb462598475af608f9934
Tobias Siegrist <tobias.siegrist@students.fhnw.ch>
2019-10-07 09:51:16 +0200

    refactored with template

--- grails-app/controllers/mvc/InPlaceCalculatorController.groovy
+++ grails-app/controllers/mvc/InPlaceCalculatorController.groovy
@@ -6,10 +6,11 @@ class InPlaceCalculatorController {
         model.en = Math.round(model.en * 10) / 10
         model.exam = Math.round(model.exam * 10) / 10
         model.result = Math.round((model.en + model.exam) / 2)
-        if (0.0 == model.en) {
+        if (0.0 == model.en && 0.0 == model.exam){
+            model.result = "Cannot calculate. Both values were invalid"
+        } else if (0.0 == model.en) {
             model.result = "Cannot calculate. En value was invalid."
-        }
-        if (0.0 == model.exam) {
+        } else if (0.0 == model.exam) {
             model.result = "Cannot calculate. Exam value was invalid."
         }
         render view: 'calc', model: [model: model]
---
+++ grails-app/views/inPlaceCalculator/_form_row.gsp
@@ -0,0 +1,5 @@
+<div>
+    <label for='en'>${label}</label>
+    <input type="number decimal" name="${namen}" value="${calculatorInstance.getProperty(name)}"
+           required="true" min="1.0" max="6.0" id="${name}"/>
+</div>
